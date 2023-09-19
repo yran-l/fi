7117041116e02a15a26cf4e2e9593061545e1370
BT_4N2_16 <@gmail.com>
2023-09-19 13:06:16 -0400

    added a line stating my favorite food

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ryan C");
         System.out.println("Wolf of Wall street");
+        System.out.println("shushi");
     }
 }
